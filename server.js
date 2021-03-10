f74a2f959ee48d8b0a4b23acd4a7d85e30124672
anagha778 <anagha778@gmail.com>
2021-03-10 18:55:45 -0500

    user login route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +14,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
